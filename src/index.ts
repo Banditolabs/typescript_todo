a35c903b07c4857376d8a71c500c4e740701567f
Banditolabs <joshuagosst@gmail.com>
2022-08-23 19:12:35 -0700

    This adds task creation

--- src/index.ts
+++ src/index.ts
@@ -13,22 +13,31 @@ type Task = {
 // getElementById will not take an element type. 
 // You must append "as 'whatever element' " to the end
 // const form = document.getElementById(".new-task-form") as HTMLFormElement | null
-const list = document.querySelector<HTMLUListElement>(".list")
-const form = document.querySelector<HTMLFormElement>(".new-task-form")
-const input = document.querySelector<HTMLInputElement>(".new-task-title")
+const list = document.querySelector<HTMLUListElement>("#list")
+const form = document.getElementById("new-task-form") as HTMLFormElement | null
+const input = document.querySelector<HTMLInputElement>("#new-task-title")
 
-
+console.log(form)
 
 // Use the ? character when the thing we're attempting to query could be null
 form?.addEventListener("submit", e => {
   e.preventDefault()
+
   if (input?.value == "" || input?.value == null) return
-  const task: Task = {
+  const newTask: Task = {
     id: uuidv4(),
     title: input.value,
     completed: false,
     createdAt: new Date()
   }
-
-  function addListItem(task: Task) {}
+  addListItem(newTask)
+  function addListItem(task: Task) {
+    const item = document.createElement("li")
+    const label = document.createElement("label")
+    const checkbox = document.createElement("input")
+    checkbox.type = "checkbox"
+    label.append(checkbox, task.title)
+    item.append(label)
+    list?.append(item)
+  }
 })
